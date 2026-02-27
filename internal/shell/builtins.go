88a6fbbb008fdd4db0429c90eb96dcdd99ee73fb
Johnny_Ma <johnny880319@gmail.com>
2026-02-27 12:05:45 +0800

    codecrafters submit [skip ci]

--- internal/shell/builtins.go
+++ internal/shell/builtins.go
@@ -97,7 +97,7 @@ func (s *Shell) checkDirectory(path string, stderr io.Writer) error {
 
 //nolint:gocognit // Will be refactored in a future exercise
 func (s *Shell) cmdHistory(args []string, _ io.Reader, stdout io.Writer, stderr io.Writer) error {
-	if args[0] == "-r" {
+	if len(args) > 1 && args[0] == "-r" {
 		historyFilePath := filepath.Join(s.workingDir, args[1])
 		//nolint:gosec // A shell's intended behavior is to open files specified by the user
 		file, err := os.Open(historyFilePath)
